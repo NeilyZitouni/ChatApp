e190ef503f89a88fdd572bc6d60f9a18bb6101c5
Neil Zitouni <nn_zitouni@esi.dz>
2025-09-21 16:29:52 +0200

    fixed the register controller

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,19 @@
+const User = require('../models/user');
+const { StatusCodes } = require('http-status-codes');
+
 const register = async (req, res) => {
-  res.send('hello there');
+  const { username, email, password } = req.body;
+  const newUser = await User.create({ username, email, password });
+  const accessToken = newUser.createAccessToken();
+  const refreshToken = newUser.createRefreshToken();
+  res.cookie('refreshToken', refreshToken, {
+    httpOnly: true,
+    //secure: true, should be true in production
+    sameSite: 'Strict',
+  });
+  res
+    .status(StatusCodes.CREATED)
+    .json({ username: newUser.username, accessToken });
 };
 
 const login = async (req, res) => {
